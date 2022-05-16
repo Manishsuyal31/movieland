281ed73bdd8d7a58993a6acc1d3cd4684f02f532
Manish Singh Suyal <96225348+Manishsuyal31@users.noreply.github.com>
2022-05-16 20:08:39 +0530

    changes for github pages

--- src/App.js
+++ src/App.js
@@ -1,87 +1,87 @@
 import React from 'react'
-import { useEffect, useState } from 'react'
-import './App.css';
+import './App.css'
 import SearchIcon from './search.svg'
-import MovieCard from './MovieCard';
+import MovieCard from './MovieCard'
+import { useState } from 'react'
 
-const API_URL = 'https://www.omdbapi.com?apikey=a30b7806';
-const moviesInitial = [
+const API_URL = 'http://www.omdbapi.com/?apikey=a30b7806'
+const initialMovies = [
     {
-    "Title": "Italian Spiderman",
-    "Year": "2007",
-    "imdbID": "tt2705436",
-    "Type": "movie",
-    "Poster": "https://m.media-amazon.com/images/M/MV5BYjFhN2RjZTctMzA2Ni00NzE2LWJmYjMtNDAyYTllOTkyMmY3XkEyXkFqcGdeQXVyNTA0OTU0OTQ@._V1_SX300.jpg"
+        "Title": "Italian Spiderman",
+        "Year": "2007",
+        "imdbID": "tt2705436",
+        "Type": "movie",
+        "Poster": "https://m.media-amazon.com/images/M/MV5BYjFhN2RjZTctMzA2Ni00NzE2LWJmYjMtNDAyYTllOTkyMmY3XkEyXkFqcGdeQXVyNTA0OTU0OTQ@._V1_SX300.jpg"
     },
     {
-    "Title": "Superman, Spiderman or Batman",
-    "Year": "2011",
-    "imdbID": "tt2084949",
-    "Type": "movie",
-    "Poster": "https://m.media-amazon.com/images/M/MV5BMjQ4MzcxNDU3N15BMl5BanBnXkFtZTgwOTE1MzMxNzE@._V1_SX300.jpg"
+        "Title": "Superman, Spiderman or Batman",
+        "Year": "2011",
+        "imdbID": "tt2084949",
+        "Type": "movie",
+        "Poster": "https://m.media-amazon.com/images/M/MV5BMjQ4MzcxNDU3N15BMl5BanBnXkFtZTgwOTE1MzMxNzE@._V1_SX300.jpg"
     },
     {
-    "Title": "Spiderman",
-    "Year": "1990",
-    "imdbID": "tt0100669",
-    "Type": "movie",
-    "Poster": "N/A"
+        "Title": "Spiderman",
+        "Year": "1990",
+        "imdbID": "tt0100669",
+        "Type": "movie",
+        "Poster": "N/A"
     },
     {
-    "Title": "Spiderman",
-    "Year": "2010",
-    "imdbID": "tt1785572",
-    "Type": "movie",
-    "Poster": "N/A"
+        "Title": "Spiderman",
+        "Year": "2010",
+        "imdbID": "tt1785572",
+        "Type": "movie",
+        "Poster": "N/A"
     },
     {
-    "Title": "Spiderman and Grandma",
-    "Year": "2009",
-    "imdbID": "tt1433184",
-    "Type": "movie",
-    "Poster": "https://m.media-amazon.com/images/M/MV5BMjE3Mzg0MjAxMl5BMl5BanBnXkFtZTcwNjIyODg5Mg@@._V1_SX300.jpg"
+        "Title": "Spiderman and Grandma",
+        "Year": "2009",
+        "imdbID": "tt1433184",
+        "Type": "movie",
+        "Poster": "https://m.media-amazon.com/images/M/MV5BMjE3Mzg0MjAxMl5BMl5BanBnXkFtZTcwNjIyODg5Mg@@._V1_SX300.jpg"
     },
     {
-    "Title": "Fighting, Flying and Driving: The Stunts of Spiderman 3",
-    "Year": "2007",
-    "imdbID": "tt1132238",
-    "Type": "movie",
-    "Poster": "https://m.media-amazon.com/images/M/MV5BNTI3NDE1ZmEtMTRiMS00YTY4LTk0OGItNjY4YmI0MDM4OGM4XkEyXkFqcGdeQXVyODE2NDgwMzM@._V1_SX300.jpg"
+        "Title": "Fighting, Flying and Driving: The Stunts of Spiderman 3",
+        "Year": "2007",
+        "imdbID": "tt1132238",
+        "Type": "movie",
+        "Poster": "https://m.media-amazon.com/images/M/MV5BNTI3NDE1ZmEtMTRiMS00YTY4LTk0OGItNjY4YmI0MDM4OGM4XkEyXkFqcGdeQXVyODE2NDgwMzM@._V1_SX300.jpg"
     },
     {
-    "Title": "The Amazing Spiderman T4 Premiere Special",
-    "Year": "2012",
-    "imdbID": "tt2233044",
-    "Type": "movie",
-    "Poster": "N/A"
+        "Title": "The Amazing Spiderman T4 Premiere Special",
+        "Year": "2012",
+        "imdbID": "tt2233044",
+        "Type": "movie",
+        "Poster": "N/A"
     },
     {
-    "Title": "Spiderman in Cannes",
-    "Year": "2016",
-    "imdbID": "tt5978586",
-    "Type": "movie",
-    "Poster": "https://m.media-amazon.com/images/M/MV5BZDlmMGQwYmItNTNmOS00OTNkLTkxNTYtNDM3ZWVlMWUyZDIzXkEyXkFqcGdeQXVyMTA5Mzk5Mw@@._V1_SX300.jpg"
+        "Title": "Spiderman in Cannes",
+        "Year": "2016",
+        "imdbID": "tt5978586",
+        "Type": "movie",
+        "Poster": "https://m.media-amazon.com/images/M/MV5BZDlmMGQwYmItNTNmOS00OTNkLTkxNTYtNDM3ZWVlMWUyZDIzXkEyXkFqcGdeQXVyMTA5Mzk5Mw@@._V1_SX300.jpg"
     },
     {
-    "Title": "Amazing Spiderman Syndrome",
-    "Year": "2012",
-    "imdbID": "tt2586634",
-    "Type": "movie",
-    "Poster": "N/A"
+        "Title": "Amazing Spiderman Syndrome",
+        "Year": "2012",
+        "imdbID": "tt2586634",
+        "Type": "movie",
+        "Poster": "N/A"
     },
     {
-    "Title": "Hollywood's Master Storytellers: Spiderman Live",
-    "Year": "2006",
-    "imdbID": "tt2158533",
-    "Type": "movie",
-    "Poster": "N/A"
+        "Title": "Hollywood's Master Storytellers: Spiderman Live",
+        "Year": "2006",
+        "imdbID": "tt2158533",
+        "Type": "movie",
+        "Poster": "N/A"
     }
-    ]
-    
+]
+
 const App = () => {
 
-    const [movies, setMovies] = useState([moviesInitial])
-    const [searchTerm, setSearchTerm] = useState([])
+    const [movies, setMovies] = useState(initialMovies);
+    const [searchTerm, setSearchTerm] = useState([]);
 
     const searchMovies = async (title) => {
         const response = await fetch(`${API_URL}&s=${title}`);
@@ -90,29 +90,16 @@ const App = () => {
         setMovies(data.Search)
     }
 
-    useEffect(() => {
-        searchMovies('Spiderman')
-    }, []);
-
     return (
         <div className='app'>
-            <h1>MovieLand</h1>
+            <h1>Moiveland</h1>
 
             <div className='search'>
-                <input
-                    placeholder='Search for movies'
-                    value={searchTerm}
-                    onChange={(event) => setSearchTerm(event.target.value)}
-                />
-                <img
-                    src={SearchIcon}
-                    alt='search'
-                    onClick={() => searchMovies(searchTerm)}
-                />
+                <input placeholder='Search for movies' onChange={(event) => (setSearchTerm(event.target.value))} />
+                <img src={SearchIcon} alt='search' onClick={() => searchMovies(searchTerm)} />
             </div>
-
             {
-                movies?.length > 0 ? (
+                movies.length > 0 ? (
                     <div className="container">
                         {movies.map((movie) => (
                             <MovieCard key={movie.imdbID} movie={movie} />
